0c36cbcc971a6392ce06c11aa71d08c1901a5796
Jack Geati <jack.geati@aol.com>
2018-04-03 14:46:16 -0500

    listqueryusers now shows whether or not the user has been approved

---
+++ src/java/src/queryusers.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Driver;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+/**
+ *
+ * @author Jack
+ */
+
+@ManagedBean   
+@SessionScoped
+public class queryusers{
+    private String firstName;
+    private String lastName;
+    private String zipcode;
+    private String ssn;
+    public static ArrayList<VoterDetails> list = new ArrayList();
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
+    String SQL;
+    List<String> enterAsOptions;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+    
+    public List getList(){
+        return list;
+    }
+    
+    public String getssn(){
+        return ssn;
+    }
+    
+    public void setssn(String ssn){
+        this.ssn = ssn;
+    }
+    
+    public String queryData(){
+        int zip = Integer.parseInt(zipcode);
+        return dbData(firstName, lastName, ssn, zip);
+    }
+    public String dbData(String firstName, String lastName, String ssn, int zipcode){
+        VoterDetails v;
+        list = new ArrayList<>();
+        try{
+            //System.out.println("Attermpting connection to database");
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sca","root","");
+            statement = connection.createStatement(); 
+            SQL = "Select * from user01 where first_name like ('" + firstName +"') and last_name like ('" + lastName + "') and ssn like ('" + ssn + "') and zip like ('" + zipcode + "')";
+            resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                    v = new VoterDetails();
+                    v.setUid(resultSet.getString(1));
+                    v.setUserFirstName(resultSet.getString(2));
+                    v.setUserLastName(resultSet.getString(3));
+                    v.setUserEmailId(resultSet.getString(4));
+                    v.setUserSSN(resultSet.getString(5));
+                    v.setUserStreetAddress(resultSet.getString(6));
+                    v.setUserCity(resultSet.getString(7));
+                    v.setUserState(resultSet.getString(8));
+                    v.setUserZip(resultSet.getString(9));  
+                    v.setUserApproved(resultSet.getString(11));
+                    list.add(v);
+            } 
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            //System.out.println("Exception occured in the process");
+            }
+            finally {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }    
+            }
+    return "listqueryusers";
+    }
+}
+    
+    
+    
+
