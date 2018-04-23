cb1959bdf075497e176d4f24754a57033d18297d
Owner <diego-castaneda@uiowa.edu>
2018-04-23 12:14:37 -0500

    finished user verification for forgot password. Next step get user security questions and change password.

--- src/java/src/verifyIdentity.java
+++ src/java/src/verifyIdentity.java
@@ -5,10 +5,70 @@
  */
 package src;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 /**
  *
  * @author Owner
  */
-public class verifyIdentity {
+
+@ManagedBean
+@SessionScoped
+public class verifyIdentity extends ElectionDBUtil{
+    private String uidToVerify;
+    private String emailToVerify;
     
+    /**
+     * @return the uidToVerify
+     */
+    public String getUidToVerify() {
+        return uidToVerify;
+    }
+
+    /**
+     * @param uidToVerify the uidToVerify to set
+     */
+    public void setUidToVerify(String uidToVerify) {
+        this.uidToVerify = uidToVerify;
+    }
+
+    /**
+     * @return the emailToVerify
+     */
+    public String getEmailToVerify() {
+        return emailToVerify;
+    }
+
+    /**
+     * @param emailToVerify the emailToVerify to set
+     */
+    
+    public void setEmailToVerify(String emailToVerify) {
+        this.emailToVerify = emailToVerify;
+    }
+
+
+    public String verify() {
+        try {
+            statement = connect().createStatement();
+            SQL = "Select * from userreg where userid like ('" + uidToVerify +"')";
+            resultSet = statement.executeQuery(SQL);
+            
+            if(!resultSet.isBeforeFirst()) { // user not found
+                return "verifyIdentityFailure";
+            } else { // user exists
+                resultSet.next();
+                System.out.println("resultSet(4): " + resultSet.getString(4) + "\nemailToVerify: " + emailToVerify + "\n");
+                if(resultSet.getString(4).equals(emailToVerify)) {
+                    return "success_reg";
+                } else {
+                    return "verifyIdentityFailure";
+                }
+            }
+            
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return "success";
+    }
 }
